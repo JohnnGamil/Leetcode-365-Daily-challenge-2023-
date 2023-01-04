f8183d45d0a75860b5265c1134f72fb3410b373d
JohnnGamil <johnngamil@gmail.com>
2023-01-05 01:31:58 +0200

    Day 4 Problem

---
+++ 1.January/Day-4(2244-Minimum-Rounds-to-Complete-All-Tasks).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        map<int,int>mp;
+        for(int i=0;i<tasks.size();i++){
+            mp[tasks[i]]++;
+        }
+        int ans = 0;
+        for(auto x:mp){
+            if(x.second == 1) return -1;
+            else{
+                if(x.second%3 == 0){
+                    ans+=x.second/3;
+                }
+                else{
+                    ans+=(x.second/3)+1;
+                }
+            }
+        }
+        return ans;
+    }
+};
